bde765a174c789c888da31d0928cd0bda5805592
Junseo Choi <130947598+ddinggul@users.noreply.github.com>
2024-10-06 17:05:16 +0900

    Title: 특정 조건 세 정수 비교, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241006/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.py
@@ -0,0 +1,16 @@
+arr = input().split()
+a = int(arr[0])
+b = int(arr[1])
+c = int(arr[2])
+
+if a < b and a < c:
+    a_1 = 1
+else:
+    a_1 = 0
+
+if a == b == c:
+    a_2 = 1
+else:
+    a_2 = 0
+
+print(f"{a_1} {a_2}")
