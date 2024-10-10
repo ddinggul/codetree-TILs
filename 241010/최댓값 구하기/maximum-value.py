a4ac59eea500a72630ed608b94f4b0b4a02ebe7d
Junseo Choi <130947598+ddinggul@users.noreply.github.com>
2024-10-10 18:31:39 +0900

    Title: 최댓값 구하기, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241010/최댓값 구하기/maximum-value.py
+++ 241010/최댓값 구하기/maximum-value.py
@@ -4,19 +4,16 @@ a = arr[0]; b = arr[1]; c = arr[2]
 result = 0
 
 if a >= b:
+    result = a
     if a >= c:
         result = a
     else:
         result = c
-elif b >= a:
+else:
+    result = b
     if b >= c:
         result = b
     else:
         result = c
-elif c >= a:
-    if c >= b:
-        result = c
-    else:
-        result = b
 
 print(result)
